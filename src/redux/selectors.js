154991bb21dcc666d0c94cdd59c9d97c4005b6de
Irina Khorunz <irahorunz87@gmail.com>
2023-01-09 13:26:37 +0200

    add optimization

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,13 +1,15 @@
+import { createSelector } from '@reduxjs/toolkit';
+
 export const selectContacts = state => state.contacts.items;
 export const selectFilter = state => state.filter.value;
 export const selectIsLoading = state => state.contacts.isLoading;
 export const selectError = state => state.contacts.error;
 
-export const selectVisibleContacts = state => {
-  const contacts = selectContacts(state);
-  const value = selectFilter(state);
-
-  return contacts.filter(contact =>
-    contact.name.toLocaleLowerCase().includes(value.toLocaleLowerCase())
-  );
-};
+export const selectVisibleContacts = createSelector(
+  [selectContacts, selectFilter],
+  (contacts, value) => {
+    return contacts.filter(contact =>
+      contact.name.toLocaleLowerCase().includes(value.toLocaleLowerCase())
+    );
+  }
+);
